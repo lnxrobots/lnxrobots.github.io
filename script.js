bee43b54dad748f28d84f580bf63bbe1d2783639
Matus Misiak <mato.mat111@gmail.com>
2023-07-21 16:05:02 +0200

    Add Slovak language

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const TABS = document.getElementsByClassName("my-tab");
 const TAB_LINKS = document.getElementsByClassName("tab-link");
+const LANG_SELECT = document.getElementById("lang-select");
+const NAVBAR = document.getElementById("my-navbar");
+
+const LANGUAGES = ['en', 'sk']
+const LANGUAGE_FILES = {'sk': 'sk.json', 'sk-SK': 'sk.json'}
+const LANGUAGE_NAMES = {'en': '🇬🇧 EN', 'sk': '🇸🇰 SK'}
 
 function removeHash() {
     history.pushState("", document.title, window.location.pathname + window.location.search);
@@ -20,10 +26,25 @@ function openTab(tab) {
     }
 }
 
+function translate(lang) {
+    LANG_SELECT.value = lang;
+    if (lang === "en") return;
+    fetch("/localization/"+LANGUAGE_FILES[lang])
+        .then(response => response.json())
+        .then(data => {
+            for(let e of document.getElementsByTagName("*")) {
+                if (e.innerHTML.trim() in data) {
+                    e.innerHTML = data[e.innerHTML.trim()];
+                    e.lang = lang;
+                }
+            }
+        });
+}
+
 for (let l of TAB_LINKS) {
     l.addEventListener("click", () => {
         openTab(l.id.split("-")[1]);
-        document.getElementById("my-navbar").classList.remove("show");
+        NAVBAR.classList.remove("show");
     });
 }
 
@@ -31,4 +52,26 @@ if (window.location.hash) {
     openTab(window.location.hash.substring(1));
 } else {
     openTab("home");
+}
+
+for (let l of LANGUAGES) {
+    let opt = document.createElement("option");
+    opt.value = l;
+    opt.innerHTML = LANGUAGE_NAMES[l];
+    LANG_SELECT.appendChild(opt);
+}
+LANG_SELECT.addEventListener("change", () => {
+    localStorage.setItem("lang", LANG_SELECT.value);
+    translate(LANG_SELECT.value);
+    if (LANG_SELECT.value === "en") {
+        window.location.reload();
+    } else {
+        NAVBAR.classList.remove("show");
+    }
+})
+
+if (localStorage.getItem("lang") != null) {
+    translate(localStorage.getItem("lang"));
+} else if (navigator.language in LANGUAGE_FILES) {
+    translate(navigator.language);
 }
